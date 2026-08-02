d5744fba0ec5370ed52aa0194cb2ce71a9b84a70
Shreya Singh <shreya08079@gmail.com>
2026-08-03 00:41:45 +0530

    Top Brains questions

---
+++ A_Top Brains/ArithmeticExpression/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Question6
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string? input = Console.ReadLine(); //Input
+
+            //Checking for null input
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Error:InvalidExpression");
+                return;
+            }
+
+            //Spliting the string input into valid parts
+            string[] parts = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            if (parts.Length != 3)  //Must equal to 3 tokens : num1 , operation, num2
+            {
+                Console.WriteLine("Error:InvalidExpression");
+                return;
+            }
+
+            if (!int.TryParse(parts[0], out int a) || !int.TryParse(parts[2], out int b))
+            {
+                Console.WriteLine("Error:InvalidNumber");
+                return;
+            }
+
+            string op = parts[1];
+
+            switch (op)
+            {
+                case "+":
+                    Console.WriteLine((a + b).ToString());
+                    break;
+
+                case "-":
+                    Console.WriteLine((a - b).ToString());
+                    break;
+
+                case "*":
+                    Console.WriteLine((a * b).ToString());
+                    break;
+
+                case "/":
+                    if (b == 0)
+                    {
+                        Console.WriteLine("Error:DivideByZero");
+                    }
+                    else
+                    {
+                        Console.WriteLine((a / b).ToString());
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Error:UnknownOperator");
+                    break;
+            }
+        }
+    }
+}
---
+++ A_Top Brains/MultiplicationTable/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Question2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                Console.Write("Enter Number N : "); //input for number
+                if (!int.TryParse(Console.ReadLine(), out int n))   //making sure string is converted into int or not
+                    throw new Exception("Invalid input for N");
+
+                Console.Write("Enter Number Upto : ");  //input for upto
+                if (!int.TryParse(Console.ReadLine(), out int upto)  || upto<=0)    //checking edge cases
+                    throw new Exception("Invalid input for Upto");
+
+                int[] multiplicationRow = Multiplication(n, upto);
+
+                foreach (int item in multiplicationRow)
+                {
+                    Console.Write(item + " ");
+                }
+            }
+            catch (Exception ecp)
+            {
+                Console.WriteLine($"Error Message : {ecp.Message}");
+            }
+
+        }
+
+        //Helper static method to return the multiplication row
+        public static int[] Multiplication(int n, int upto)
+        {
+            int[] multiplicationRow = new int[upto];
+            for (int i = 1; i <= upto; i++)
+            {
+                multiplicationRow[i - 1] = n * i;
+            }
+
+            return multiplicationRow;
+        }
+    }
+}
